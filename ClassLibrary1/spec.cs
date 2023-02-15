d4a1b482fa3ed07aac68cabf963f4fc2020f238d
foxline3 <125448874+foxline3@users.noreply.github.com>
2023-02-15 12:57:54 +0300

    Add files via upload

---
+++ ClassLibrary1/spec.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace spec
+{
+    class spec
+    {
+        private string name;
+        private string reduction;
+
+        public spec(spec name, spec reduction)
+        {
+            string Name;
+            string Reduction;
+
+            name = name;
+            reduction = reduction;
+        }
+    }
+}
